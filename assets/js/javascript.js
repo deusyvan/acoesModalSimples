495dfa33831cfd37ce17321c57e6d6693bbcb442
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2019-01-04 10:04:33 -0200

    Desacoplando as funções

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -14,35 +14,36 @@ function editar(id){
 			
 			//Procuramos dentro do modal-body um form que está lá dentro
 			//Depois acionamos uma ação submit, que vai executar uma função que recebe e
-			$('#modal').find('.modal-body').find('form').on('submit',function(e){
-				//Previne o evento padrão que ocorre
-				e.preventDefault();
-				
-				//Pegar os dados do form manualmente
-				var nome = $(this).find('input[name=nome]').val();
-				var email = $(this).find('input[name=email]').val();
-				var senha = $(this).find('input[name=senha]').val();
-				var id = $(this).find('input[name=id]').val();
-				
-				//Fazer nova requisição ajax para enviar a um arquivo que vai salvar no banco de dados
-				$.ajax({
-					url:'salvar.php',
-					type:'POST',
-					data:{nome:nome, email:email, senha:senha, id:id},
-					success:function(){
-						//Mais uma função após ter sido salvo no banco de dados.
-						alert("Dados alterados com sucesso");
-						//fechando o modal
-						$('#modal').modal('hide');
-					}
-				});
-				
-			});
+			$('#modal').find('.modal-body').find('form').on('submit', salvar);
 			$('#modal').modal('show');
 		}
 		
 	});
 		
+}
+
+function salvar(e){
+	//Previne o evento padrão que ocorre
+	e.preventDefault();
+	
+	//Pegar os dados do form manualmente
+	var nome = $(this).find('input[name=nome]').val();
+	var email = $(this).find('input[name=email]').val();
+	var senha = $(this).find('input[name=senha]').val();
+	var id = $(this).find('input[name=id]').val();
+	
+	//Fazer nova requisição ajax para enviar a um arquivo que vai salvar no banco de dados
+	$.ajax({
+		url:'salvar.php',
+		type:'POST',
+		data:{nome:nome, email:email, senha:senha, id:id},
+		success:function(){
+			//Mais uma função após ter sido salvo no banco de dados.
+			alert("Dados alterados com sucesso");
+			//fechando o modal
+			$('#modal').modal('hide');
+		}
+	});
 }
 
 function excluir(id){
