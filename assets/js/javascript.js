2288de41b6e58fe299df7d473687a4cb168acd2e
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2019-01-04 09:53:55 -0200

    Agora vamos evitar que o formulario seja enviado ao index, poi dessa nova forma ele fica no index e não no editar.php

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -9,8 +9,24 @@ function editar(id){
 			$('#modal').modal('show');
 		},
 		success:function(html){
-			//Pegando o html para colocar dentro do modal
+			//Pegando o html para colocar dentro do modal (na tela)
 			$('#modal').find('.modal-body').html(html);
+			
+			//Procuramos dentro do modal-body um form que está lá dentro
+			//Depois acionamos uma ação submit, que vai executar uma função que recebe e
+			$('#modal').find('.modal-body').find('form').on('submit',function(e){
+				//Previne o evento padrão que ocorre
+				e.preventDefault();
+				
+				//Pegar os dados do form manualmente
+				var nome = $(this).find('input[name=nome]').val();
+				var email = $(this).find('input[name=email]').val();
+				var senha = $(this).find('input[name=senha]').val();
+				
+				//Fazer nova requisição ajax para enviar a um arquivo que vai salvar no banco de dados
+				
+				
+			});
 			$('#modal').modal('show');
 		}
 		
